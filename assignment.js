572339507ba3530f25b53b32ee99582667ca007f
ahanafabdullah9@gmail.com <ahanafabdullah9@gmail.com>
2021-01-22 19:40:39 +0600

    Finished

--- assignment.js
+++ assignment.js
@@ -1,12 +1,15 @@
-//  function kilometerToMeter( distance ) {
-//     var meter = distance * 1000;
-//     return meter;
-// }
-// var result = kilometerToMeter(34);
-// console.log(result);
+//https://github.com/AhanafVai/assignment.js
 
 
-function woodCalculator(clock, phone, laptop){
+ function kilometerToMeter( distance ) {
+    var meter = distance * 1000;
+    return meter;
+}
+var convert = kilometerToMeter(34);
+console.log(convert);
+
+
+ function budgetCalculator(clock, phone, laptop){
     const clockCost = clock * 50;
     const phoneCost = phone * 100;
     const laptopCost = laptop * 500;
@@ -14,43 +17,43 @@ function woodCalculator(clock, phone, laptop){
     let totalcost = clockCost+phoneCost+laptopCost;
     return totalcost; 
 }
- let result= woodCalculator(2,2,2)
- console.log(result);
+ let sum= budgetCalculator(2,2,2)
+ console.log(sum);
 
 
 
-// 3.function hotelCost(day){
-//     var cost = 0;
-//     if(day < 11){
-//         cost = day*100;
-//     }
-//     else if(day < 21){
-//         var first10days = 10*100;
-//         var remaindays = day - 10;
-//         var second10days = remaindays*80;
-//         cost = first10days + second10days;
-//     }
-//     else{
-//         var first10days = 10*100;
-//         var second10days = 10*80;
-//         var remaindays = day - 20;
-//         var afterthat = remaindays*50;
-//         cost = afterthat + first10days + second10days;
-//     }
-//     return cost;
-// }
-// var result = hotelCost(30);
-// console.log(result);
+function hotelCost(day){
+    var cost = 0;
+    if(day < 11){
+        cost = day*100;
+    }
+    else if(day < 21){
+        var first10days = 10*100;
+        var remaindays = day - 10;
+        var second10days = remaindays*80;
+        cost = first10days + second10days;
+    }
+    else{
+        var first10days = 10*100;
+        var second10days = 10*80;
+        var remaindays = day - 20;
+        var afterthat = remaindays*50;
+        cost = afterthat + first10days + second10days;
+    }
+    return cost;
+}
+var bill = hotelCost(30);
+console.log(bill);
 
-// 4. function megaFriend(arr =["rafi","rafina","rokksana"]) {
-//     let word = "";
-//     for (let i = 0; i < arr.length; i++) {
-//       if (word.length < arr[i].length) {
-//         word = arr[i];
-//       }
-//     }
-//     return word.length;
-//   }
-// var result = megaFriend();
-// console.log(result);
+ function megaFriend(arr =["rafi","rafina","rokksana"]) {
+    let word = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (word.length < arr[i].length) {
+        word = arr[i];
+      }
+    }
+    return word.length;
+  }
+var result = megaFriend();
+console.log(result);
 
